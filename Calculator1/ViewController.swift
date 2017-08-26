54b3b00494a722b8053d3a983de414143831fa8b
Fukada.lmno <fukada.lmno@gmail.com>
2017-08-27 01:19:21 +0900

    電卓つくってみた

--- Calculator1/ViewController.swift
+++ Calculator1/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var text_label: UILabel!
+    @IBOutlet weak var prev_num_label: UILabel!
+    @IBOutlet weak var calc_label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        text_label.text = ""
+        prev_num_label.text = ""
+        calc_label.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func num_button_pushed(_ sender: UIButton) {
+        text_label.text! += sender.titleLabel!.text!
+    }
+    
+    @IBAction func calc_sighn_pushed(_ sender: UIButton) {
+        calc_label.text = sender.titleLabel!.text!
+        prev_num_label.text = text_label.text
+        text_label.text = ""
+    }
+    
+    @IBAction func equal_pushed(_ sender: Any) {
+        if prev_num_label.text != nil && text_label.text != nil {
+            if calc_label.text == "+" {
+                text_label.text = String(Int(prev_num_label.text!)! + Int(text_label.text!)!)
+            } else if calc_label.text == "-" {
+                text_label.text = String(Int(prev_num_label.text!)! - Int(text_label.text!)!)
+            } else if calc_label.text == "*" {
+                text_label.text = String(Int(prev_num_label.text!)! * Int(text_label.text!)!)
+            } else if calc_label.text == "*" {
+                text_label.text = String(Int(prev_num_label.text!)! / Int(text_label.text!)!)
+            }
+        }
+    }
+    
 }
 
