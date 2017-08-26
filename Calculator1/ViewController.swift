59ffe398304d7e0f721a728ce3ae69508cc49d68
Fukada.lmno <fukada.lmno@gmail.com>
2017-08-26 19:43:56 +0900

    Initial Commit

---
+++ Calculator1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator1
+//
+//  Created by Kotaro Fukada on 2017/08/26.
+//  Copyright © 2017年 LMNO Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
